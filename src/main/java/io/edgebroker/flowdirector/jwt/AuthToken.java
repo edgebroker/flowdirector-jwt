2aaabf1108fc7bf2ac14db89d1cbe91eada35c38
Mike <reach@sysdevmike.com>
2019-07-29 18:18:30 +0900

    Allow different secret and issuer per token.

--- src/main/java/io/edgebroker/flowdirector/jwt/AuthToken.java
+++ src/main/java/io/edgebroker/flowdirector/jwt/AuthToken.java
@@ -7,40 +7,58 @@ import com.auth0.jwt.interfaces.JWTVerifier;
 
 public class AuthToken {
 
-    protected static Algorithm algorithm = Algorithm.HMAC256("secret");
-    protected static String issuer = "flowdirector";
-    DecodedJWT decodedJwt;
+    private Algorithm algorithm;
+    private String issuer;
+    private String token;
+    private DecodedJWT decodedJwt;
 
-    public AuthToken(String token) {
+    public AuthToken(String secret, String issuer) {
+        this.algorithm = Algorithm.HMAC256(secret);
+        this.issuer = issuer;
+    }
+
+    public AuthToken setToken(String token) {
+        this.token = token;
+        return this;
+    }
+
+    private AuthToken decodeJwt() throws Exception {
+
+        if (this.token == null) {
+            throw new Exception("Token not set.");
+        }
+
         JWTVerifier verifier = JWT.require(algorithm)
                 .withIssuer(issuer)
                 .build();
         this.decodedJwt = verifier.verify(token);
+
+        return this;
     }
 
-    public static String fromId(int id) {
+    public String fromId(int id) {
         return JWT.create()
                 .withClaim("id", id)
                 .withIssuer(issuer)
                 .sign(algorithm);
     }
 
-    public int getId() {
+    public int getId() throws Exception {
+
+        if (this.decodedJwt == null) {
+            throw new Exception("JWT has not been decoded.");
+        }
+
         return decodedJwt.getClaims().get("id").asInt();
     }
 
-    private JWTVerifier createTokenVerifier() {
-        return JWT.require(this.algorithm)
-                .withIssuer(this.issuer)
-                .build();
-    }
+    public static void main(String[] args) throws Exception {
+        AuthToken authToken = new AuthToken("secret", "flowdirector");
 
-
-
-    public static void main(String[] args) {
-        String token =  AuthToken.fromId(100);
+        String token =  authToken.fromId(100);
         System.out.println(token);
-        AuthToken decodedToken = new AuthToken(token);
-        System.out.println(decodedToken.getId());
+
+        int id = authToken.setToken(token).decodeJwt().getId();
+        System.out.println(id);
     }
 }
